d40930c21bcb0303a1688a76f16495a6eb3c1e8e
albabernal03 <ALBABERO@MYUAX.COM>
2022-04-27 21:30:36 +0200

    instalo panda

---
+++ clases/__pycache__/calculos.py
@@ -0,0 +1,5 @@
+from math import * #Este módulo proporciona acceso a las funciones matemáticas
+import pandas as pd # Libreria para trabajar con dataframes (tablas)
+import mathplotlib.pyplot as plt #Para graficar
+
+datos= pd.read_csv('/Users/hectorbernaltrujillo/Documents/informática/Programación python/critica_pelicula2.0/critica_peliculas.csv') #Leer el archivo csv
